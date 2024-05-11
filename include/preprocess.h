3f02ce650ab7170fe168a4f5ccb11af584ef93f6
liunao123 <812807457@qq.com>
2024-05-11 15:04:38 +0800

    update header file and cmakelist.txt

--- include/preprocess.h
+++ include/preprocess.h
@@ -77,6 +77,22 @@ POINT_CLOUD_REGISTER_POINT_STRUCT(ouster_ros::Point,
     (std::uint32_t, range, range)
 )
 
+
+struct RsPointXYZIRT
+{
+  PCL_ADD_POINT4D;
+  float intensity;
+  // PCL_ADD_INTENSITY;
+  uint16_t ring = 0;
+  double timestamp = 0;
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+} EIGEN_ALIGN16;
+
+POINT_CLOUD_REGISTER_POINT_STRUCT(RsPointXYZIRT,
+                                  (float, x, x)(float, y, y)(float, z, z)(float, intensity, intensity)(uint16_t, ring, ring)(double, timestamp, timestamp))
+
+
+
 class Preprocess
 {
   public:
